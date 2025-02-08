c4c77d297867306ed23f502e74fec825a3fbdb33
Alfin5015 <alfinalbert10@gmail.com>
2025-02-08 19:57:40 +0530

    Update do_you_love_me.js
    fixed bug in js

--- do_you_love_me.js
+++ do_you_love_me.js
@@ -1,28 +1,32 @@
 const questionContainer = document.querySelector(".question-container");
-const resultContainer = document.querySelector(".result-container.yes");
+const resultContainer = document.querySelector(".result-container");
 const gifResult = document.querySelector(".gif-result");
 const heartLoader = document.querySelector(".cssload-main");
 const yesBtn = document.querySelector(".js-yes-btn");
 const noBtn = document.querySelector(".js-no-btn");
 
-// /change the postion of no button
+// Change the position of the no button
 noBtn.addEventListener("mouseover", () => {
   const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
-  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetHeight);
 
+  noBtn.style.position = "absolute"; // Ensure absolute positioning
   noBtn.style.left = `${newX}px`;
   noBtn.style.top = `${newY}px`;
 });
 
-// yes button functionality
-
+// Yes button functionality
 yesBtn.addEventListener("click", () => {
   questionContainer.style.display = "none";
   heartLoader.style.display = "inherit";
 
-  const timeoutId = setTimeout(() => {
+  setTimeout(() => {
     heartLoader.style.display = "none";
     resultContainer.style.display = "inherit";
+
+    // Ensure video playback
+    gifResult.load();
+    gifResult.muted = false; // Ensure it's unmuted
     gifResult.play();
   }, 3000);
 });
